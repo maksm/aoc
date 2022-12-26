ef6ef7cc4d2e4ce584245ead6e23004ff50d5782
Maks Mržek <maks.mrzek@gmail.com>
2022-12-26 20:38:06 +0000

    2015 day1 and day2

---
+++ 2015/1.py
@@ -0,0 +1,22 @@
+day = 1
+
+with open(f'2015/inputs/{day}.txt','r') as f: lines = f.read().splitlines()
+
+def count_floor(line):
+    return sum(1 for x in line if x == '(') + sum(-1 for x in line if x ==')')
+
+# part 1
+for line in lines_t:
+    print(count_floor(line))
+
+print()
+
+print(count_floor(lines[0]))
+
+# part 2
+i = 1
+line = lines[0]
+while sum(1 for x in line[:i] if x == '(') + sum(-1 for x in line[:i] if x ==')') != -1:
+    i+=1
+
+print(i)
